6b0b7a6e623b72df0168375dfd67825d7f71fabd
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-08-08 16:10:41 +0200

    [UPD] mile stone 1

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
     { name: 'Pane', price: 1.2 },
     { name: 'Latte', price: 1.0 },
     { name: 'Pasta', price: 0.7 },
-  ];
+  ]
 
   return (
     <div className='ListContainer'>
